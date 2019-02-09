59cbd4559d6979daf12ec78bc7057eb8c8e7b1ed
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-09 12:55:47 +0400

    relise one

---
+++ DoWhileFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 4;
+            int factorial = 1;
+            Console.Write("Factorial of numbre {0}! = ", counter);
+            do
+            {
+                factorial *= counter--;
+            } while (counter > 0);
+            Console.WriteLine("{0}", factorial);
+
+
+            Console.ReadKey();
+        }
+    }
+}
